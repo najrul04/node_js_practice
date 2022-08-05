9a549ee56b730da162cf1a719b172878c1378297
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-05 19:30:08 +0600

    Node_JS_Practice

--- index.js
+++ index.js
@@ -1,3 +1,7 @@
+const _ = require('lodash');
+const people = require('./people');
+
+console.log(_.last(people));
 /* const a = 5;
 console.log(a);
 
@@ -8,8 +12,13 @@ setTimeout(() => {
 setInterval(() => {
     console.log('Infinite');
 }, 5000);
- */
+
 console.log(global);
 
 console.log(__dirname);
 console.log(__filename);
+ */
+/* console.log(people.people);
+console.log(people.a);
+people.test();
+ */
---
+++ people.js
@@ -0,0 +1,16 @@
+const people = ['Sakib', 'Tamim', 'Mashrafee'];
+
+module.exports = people;
+/* const a = 6;
+function test() {
+    console.log('test Function');
+}
+test();
+console.log(module);
+
+module.exports = {
+    people,
+    a,
+    test,
+};
+ */
