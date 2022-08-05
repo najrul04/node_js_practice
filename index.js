6d33639b57c998aaef053efeec697227a3978e38
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-05 22:22:32 +0600

    Node_JS_Practice

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-const _ = require('lodash');
+/* const _ = require('lodash');
 const people = require('./people');
 
-console.log(_.last(people));
+console.log(_.last(people)); */
 /* const a = 5;
 console.log(a);
 
@@ -22,3 +22,12 @@ console.log(__filename);
 console.log(people.a);
 people.test();
  */
+
+const path = require('path');
+
+const myPath = 'F:/LWR/JavaScript Framework (Node JS)/index.js';
+
+console.log(path.basename(myPath));
+console.log(path.dirname(myPath));
+console.log(path.extname(myPath));
+console.log(path.parse(myPath));
