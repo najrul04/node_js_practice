8b38aa1418326266097220ab24cd706254ff0a14
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-06 15:22:21 +0600

    Node_JS_Practice

---
+++ lesson - 5/http.js
@@ -0,0 +1,21 @@
+const http = require('http');
+
+const server = http.createServer((req, res) => {
+    if (req.url === '/') {
+        res.write('Hello World ');
+        res.write('Hello World, What is up?');
+        res.end();
+    } else if (req.url === '/about') {
+        res.write("Oi! That's about us page!");
+        res.end();
+    } else {
+        res.write('wass upppp Not Hereeeee');
+        res.end();
+    }
+});
+
+// Server is listening at 3000 port
+
+server.listen(3000);
+
+console.log('Listening on Port 3000');
