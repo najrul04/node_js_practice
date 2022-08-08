01f15abe06e1a60094c55a752a3580f40737b814
MD Najrul Islam Sumon <nazrul2517@gmail.com>
2022-08-08 16:51:30 +0600

    Node_js_practice_small_project

--- lesson - 6/index.js
+++ lesson - 6/index.js
@@ -1,8 +1,25 @@
-const fs = require('fs');
+/* const fs = require('fs');
 
 const ourReadStream = fs.createReadStream(`${__dirname}/bigData.txt`);
 const ourWriteStream = fs.createWriteStream(`${__dirname}/outPut.txt.txt`);
 
-ourReadStream.on('data', (chunk) => {
-    ourWriteStream.write(chunk);
+//  ourReadStream.on('data', (chunk) => {
+//     ourWriteStream.write(chunk);
+// });
+
+ourReadStream.pipe(ourWriteStream);
+ */
+
+const http = require('http');
+const fs = require('fs');
+
+const server = http.createServer((req, res) => {
+    const myReadStream = fs.createReadStream(`${__dirname}/bigData.txt`, 'utf-8');
+
+    myReadStream.pipe(res);
 });
+
+// Server is listening at 3000 port
+
+server.listen(3000);
+console.log('Listening on Port 3000');
